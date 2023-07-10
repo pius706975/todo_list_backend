08b606eff1df6eb1b923343013f03b70d1e23c3e
pius <piusrestiantoro2@gmail.com>
2023-07-10 21:27:40 +0800

    DB migrate down is created

--- databases/db.migrate.go
+++ databases/db.migrate.go
@@ -16,7 +16,7 @@ var migrateUP bool
 var migrateDOWN bool
 
 func init()  {
-	MigrateCMD.Flags().BoolVarP(&migrateUP, "dbUP", "u", true, "run migration up")
+	MigrateCMD.Flags().BoolVarP(&migrateUP, "dbUP", "u", false, "run migration up")
 
 	MigrateCMD.Flags().BoolVarP(&migrateDOWN, "dbDOWN", "d", false, "run migration down")
 }
@@ -24,22 +24,26 @@ func init()  {
 func dbMigrate(cmd *cobra.Command, args []string) error {
 	DBInit()
 	defer db.Close()
-	
-	if migrateUP {
-		err := DoMigrate("up")
-		if err != nil {
-			return err
-		}
+
+	up := DoMigrateUp(migrateUP)
+	if up != nil {
+		return up
+	}
+
+	down := DoMigrateDown(migrateDOWN)
+	if down != nil {
+		return down
 	}
 
 	return nil
 }
 
-func DoMigrate(direction string) error {
-	switch direction {
-	case "up":
+func DoMigrateUp(up bool) error {
+	
+	switch up {
+	case true:
 		_, err := db.Exec(`
-			CREATE TABLE activities (
+			CREATE TABLE IF NOT EXISTS activities (
 				activity_id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
 				title VARCHAR(255),
 				email VARCHAR(255),
@@ -51,7 +55,7 @@ func DoMigrate(direction string) error {
 		}
 
 		_, err = db.Exec(`
-			CREATE TABLE todos (
+			CREATE TABLE IF NOT EXISTS todos (
 				todo_id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
 				activity_group_id BIGINT UNSIGNED DEFAULT 0,
 				title VARCHAR(255),
@@ -66,9 +70,28 @@ func DoMigrate(direction string) error {
 		if err != nil {
 			return err
 		}
+
+		log.Println("Migration up done")
+	}
+	
+	return nil
+}
+
+func DoMigrateDown(down bool) error {
+	switch down {
+	case true:
+		_, err := db.Exec("DROP TABLE todos")
+		if err != nil {
+			return err
+		}
+
+		_, err = db.Exec("DROP TABLE activities")
+		if err != nil {
+			return err
+		}
+
+		log.Println("Migration down done")
 	}
 
-	log.Println("Migration up done")
-	
 	return nil
 }
